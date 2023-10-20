26376bf09cd0ceaea82175375e408b5b7ea24514
Shaqs0 <hhgfhhhggb@gmail.com>
2023-10-20 13:48:29 +0300

    correct

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
  function validation(form) {
     let result = true;
 
+    const allInputs = form.querySelectorAll('input');
+
+    for (const input of allInputs) {
+         
+      if(input.value==""){
+         console.log('Ошибка поля')
+      }
+    }
+
     return result
  }
  
  document.getElementById('add-form').addEventListener('submit', function(event) {
-    event.preventDefault()
+    event.preventDefault();
     
     if(validation(this)===true){
-        alert('Успешно!')
+        alert('Успешно!');
     }
  })
